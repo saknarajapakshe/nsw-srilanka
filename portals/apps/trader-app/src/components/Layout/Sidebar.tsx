sthanikan2000/nsw-agency

[CI][Portals] Enforce Prettier checks and fail ESLint in Portals CI (#426)
* feat: integrate Prettier and ESLint for code formatting and linting

* chore: apply prettier formatting to existing files

frontend/src/components/Layout/Sidebar.tsx
@@ -1,14 +1,11 @@
 import { Link, useLocation } from 'react-router-dom'
-import {
-  ArchiveIcon,
-  ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon,
-} from '@radix-ui/react-icons'
-import { type ReactNode, useEffect, useRef, useState } from "react";
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { ArchiveIcon, ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons'
+import { type ReactNode, useEffect, useRef, useState } from 'react'
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
 
 interface NavItem {
@@ -18,67 +15,67 @@ interface NavItem {
 }
 
 interface NavGroup {
-  name: string;
-  icon: React.ReactNode;
-  items: NavItem[];
+  name: string
+  icon: React.ReactNode
+  items: NavItem[]
 }
 
-type NavItemOrGroup = NavItem | NavGroup;
+type NavItemOrGroup = NavItem | NavGroup
 
 const navStructure: NavItemOrGroup[] = [
   { name: 'Workflows', path: '/workflows', icon: <ArchiveIcon className="w-5 h-5" /> },
 ]
 
 function isNavGroup(item: NavItemOrGroup): item is NavGroup {
-  return 'items' in item;
+  return 'items' in item
 }
 
 interface SidebarProps {
-  isExpanded: boolean;
-  onToggle: () => void;
+  isExpanded: boolean
+  onToggle: () => void
 }
 
 export function Sidebar({ isExpanded, onToggle }: SidebarProps) {
-  const location = useLocation();
-  const [isHovered, setIsHovered] = useState(false);
-  const [expandedGroups, setExpandedGroups] = useState<Set<string>>(new Set());
-  const previousPathRef = useRef<string>(location.pathname);
+  const location = useLocation()
+  const [isHovered, setIsHovered] = useState(false)
+  const [expandedGroups, setExpandedGroups] = useState<Set<string>>(new Set())
+  const previousPathRef = useRef<string>(location.pathname)
 
   // Determine if sidebar should show expanded content
-  const showExpanded = isExpanded || (!isExpanded && isHovered);
+  const showExpanded = isExpanded || (!isExpanded && isHovered)
 
   // Auto-expand groups that contain the active page
   useEffect(() => {
     if (previousPathRef.current !== location.pathname) {
-      previousPathRef.current = location.pathname;
-      const groupsToExpand = new Set<string>();
+      previousPathRef.current = location.pathname
+      const groupsToExpand = new Set<string>()
       navStructure.forEach((item) => {
         if (isNavGroup(item)) {
-          const hasActivePath = item.items.some((child) => child.path === location.pathname);
+          const hasActivePath = item.items.some((child) => child.path === location.pathname)
           if (hasActivePath) {
-            groupsToExpand.add(item.name);
+            groupsToExpand.add(item.name)
           }
         }
-      });
+      })
       // eslint-disable-next-line react-hooks/set-state-in-effect
-      setExpandedGroups(groupsToExpand);
+      setExpandedGroups(groupsToExpand)
     }
-  }, [location.pathname]);
+  }, [location.pathname])
 
   const toggleGroup = (groupName: string) => {
     setExpandedGroups((prev) => {
-      const next = new Set(prev);
+      const next = new Set(prev)
       if (next.has(groupName)) {
-        next.delete(groupName);
+        next.delete(groupName)
       } else {
-        next.add(groupName);
+        next.add(groupName)
       }
-      return next;
-    });
-  };
+      return next
+    })
+  }
 
   const renderNavItem = (item: NavItem, isInGroup = false) => {
-    const isActive = location.pathname === item.path || (item.path !== '/' && location.pathname.startsWith(item.path));
+    const isActive = location.pathname === item.path || (item.path !== '/' && location.pathname.startsWith(item.path))
     return (
       <Link
         key={item.path}
@@ -89,28 +86,27 @@ export function Sidebar({ isExpanded, onToggle }: SidebarProps) {
             ? 'bg-primary-500 text-white shadow-md'
             : 'text-primary-100 hover:bg-primary-800/50 hover:text-white',
           !showExpanded && 'justify-center',
-          isInGroup && showExpanded && 'ml-4 text-sm'
+          isInGroup && showExpanded && 'ml-4 text-sm',
         )}
         title={!showExpanded ? item.name : undefined}
       >
         <span className="flex items-center text-xl shrink-0">{item.icon}</span>
         {showExpanded && <span className="text-[15px] whitespace-nowrap">{item.name}</span>}
       </Link>
-    );
-  };
+    )
+  }
 
   const renderNavGroup = (group: NavGroup) => {
-    const isGroupExpanded = expandedGroups.has(group.name);
-    const hasActivePath = group.items.some((item) => item.path === location.pathname);
+    const isGroupExpanded = expandedGroups.has(group.name)
+    const hasActivePath = group.items.some((item) => item.path === location.pathname)
 
     if (!showExpanded) {
       // Collapsed sidebar: show group header and expanded sub-items with shared background
       return (
         <div key={group.name} className="flex flex-col gap-1">
-          <div className={cn(
-            'flex flex-col gap-1 rounded-md transition-all',
-            isGroupExpanded && 'bg-primary-500/20 p-1'
-          )}>
+          <div
+            className={cn('flex flex-col gap-1 rounded-md transition-all', isGroupExpanded && 'bg-primary-500/20 p-1')}
+          >
             <button
               onClick={() => toggleGroup(group.name)}
               className={cn(
@@ -119,7 +115,7 @@ export function Sidebar({ isExpanded, onToggle }: SidebarProps) {
                   ? 'text-white hover:bg-primary-500/40 border-primary-400/30'
                   : hasActivePath
                     ? 'bg-primary-500/30 text-white border-primary-400/20'
-                    : 'text-primary-100 hover:bg-primary-800/50 hover:text-white border-transparent hover:border-primary-600/20'
+                    : 'text-primary-100 hover:bg-primary-800/50 hover:text-white border-transparent hover:border-primary-600/20',
               )}
               title={group.name}
             >
@@ -127,32 +123,33 @@ export function Sidebar({ isExpanded, onToggle }: SidebarProps) {
               <ChevronDownIcon
                 className={cn(
                   'w-3 h-3 absolute bottom-1 right-1 transition-transform',
-                  isGroupExpanded ? 'rotate-0' : '-rotate-90'
+                  isGroupExpanded ? 'rotate-0' : '-rotate-90',
                 )}
               />
             </button>
 
-            {isGroupExpanded && group.items.map((item) => {
-              const isActive = location.pathname === item.path;
-              return (
-                <Link
-                  key={item.path}
-                  to={item.path}
-                  className={cn(
-                    'flex items-center justify-center px-3 h-12 min-h-12 shrink-0 rounded-md transition-all',
-                    isActive
-                      ? 'bg-primary-500 text-white shadow-md'
-                      : 'text-primary-100 hover:bg-primary-500/40 hover:text-white'
-                  )}
-                  title={item.name}
-                >
-                  <span className="flex items-center text-xl shrink-0">{item.icon}</span>
-                </Link>
-              );
-            })}
+            {isGroupExpanded &&
+              group.items.map((item) => {
+                const isActive = location.pathname === item.path
+                return (
+                  <Link
+                    key={item.path}
+                    to={item.path}
+                    className={cn(
+                      'flex items-center justify-center px-3 h-12 min-h-12 shrink-0 rounded-md transition-all',
+                      isActive
+                        ? 'bg-primary-500 text-white shadow-md'
+                        : 'text-primary-100 hover:bg-primary-500/40 hover:text-white',
+                    )}
+                    title={item.name}
+                  >
+                    <span className="flex items-center text-xl shrink-0">{item.icon}</span>
+                  </Link>
+                )
+              })}
           </div>
         </div>
-      );
+      )
     }
 
     return (
@@ -163,38 +160,36 @@ export function Sidebar({ isExpanded, onToggle }: SidebarProps) {
             'flex items-center gap-4 px-3 h-12 min-h-12 shrink-0 rounded-md font-medium transition-all w-full',
             hasActivePath && isGroupExpanded
               ? 'bg-primary-500/20 text-white'
-              : 'text-primary-100 hover:bg-primary-800/50 hover:text-white'
+              : 'text-primary-100 hover:bg-primary-800/50 hover:text-white',
           )}
         >
           <span className="flex items-center text-xl shrink-0">{group.icon}</span>
           <span className="text-[15px] whitespace-nowrap flex-1 text-left">{group.name}</span>
           <ChevronDownIcon
-            className={cn(
-              'w-4 h-4 transition-transform',
-              isGroupExpanded ? 'rotate-0' : '-rotate-90'
-            )}
+            className={cn('w-4 h-4 transition-transform', isGroupExpanded ? 'rotate-0' : '-rotate-90')}
           />
         </button>
         {isGroupExpanded && (
           <div className="flex flex-col gap-1">{group.items.map((item) => renderNavItem(item, true))}</div>
         )}
       </div>
-    );
-  };
+    )
+  }
 
   return (
     <aside
-      className={`${showExpanded ? 'w-64' : 'w-20'
-        } h-[calc(100vh-64px)] bg-linear-to-b from-primary-900 to-primary-950 text-white flex flex-col fixed left-0 top-16 border-r border-primary-800/30 shadow-xl transition-all duration-300 z-20`}
+      className={`${
+        showExpanded ? 'w-64' : 'w-20'
+      } h-[calc(100vh-64px)] bg-linear-to-b from-primary-900 to-primary-950 text-white flex flex-col fixed left-0 top-16 border-r border-primary-800/30 shadow-xl transition-all duration-300 z-20`}
       onMouseEnter={() => !isExpanded && setIsHovered(true)}
       onMouseLeave={() => !isExpanded && setIsHovered(false)}
     >
       <nav className="flex-1 p-3 flex flex-col gap-1 overflow-y-auto">
         {navStructure.map((item) => {
           if (isNavGroup(item)) {
-            return renderNavGroup(item);
+            return renderNavGroup(item)
           }
-          return renderNavItem(item);
+          return renderNavItem(item)
         })}
       </nav>
 
@@ -209,32 +204,23 @@ export function Sidebar({ isExpanded, onToggle }: SidebarProps) {
             </div>
           </div>
         )}
-        {!showExpanded && (
-          <div className="p-4 flex justify-center">
-          </div>
-        )}
+        {!showExpanded && <div className="p-4 flex justify-center"></div>}
 
         <div className="px-4 pb-4">
           <button
             onClick={onToggle}
-            className={`${showExpanded ? 'w-full' : 'w-10'
-              } h-10 rounded-full bg-primary-500 hover:bg-primary-600 flex items-center ${showExpanded ? 'justify-between px-4' : 'justify-center'
-              } text-white transition-all shadow-lg`}
-            title={isExpanded ? "Collapse sidebar" : "Expand sidebar"}
+            className={`${
+              showExpanded ? 'w-full' : 'w-10'
+            } h-10 rounded-full bg-primary-500 hover:bg-primary-600 flex items-center ${
+              showExpanded ? 'justify-between px-4' : 'justify-center'
+            } text-white transition-all shadow-lg`}
+            title={isExpanded ? 'Collapse sidebar' : 'Expand sidebar'}
           >
-            {showExpanded && (
-              <span className="text-sm font-medium">
-                {isExpanded ? 'Collapse' : 'Expand'}
-              </span>
-            )}
-            {isExpanded ? (
-              <ChevronLeftIcon className="w-5 h-5" />
-            ) : (
-              <ChevronRightIcon className="w-5 h-5" />
-            )}
+            {showExpanded && <span className="text-sm font-medium">{isExpanded ? 'Collapse' : 'Expand'}</span>}
+            {isExpanded ? <ChevronLeftIcon className="w-5 h-5" /> : <ChevronRightIcon className="w-5 h-5" />}
           </button>
         </div>
       </div>
     </aside>
-  );
+  )
 }

saknarajapakshe/nsw-srilanka

portals/apps/trader-app/src/components/Layout/Sidebar.tsx
@@ -1,6 +1,6 @@
 import { Link, useLocation } from 'react-router-dom'
 import { DashboardIcon, FileTextIcon, ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons'
-import { type ReactNode, useEffect, useRef, useState, useMemo } from "react";
+import { type ReactNode, useEffect, useRef, useState, useMemo } from 'react'
 import { useRole, type Role } from '../../services/RoleContext'
 
 interface NavItem {
@@ -11,13 +11,13 @@ interface NavItem {
 }
 
 interface NavGroup {
-  name: string;
-  icon: React.ReactNode;
-  items: NavItem[];
+  name: string
+  icon: React.ReactNode
+  items: NavItem[]
   roles?: Role[]
 }
 
-type NavItemOrGroup = NavItem | NavGroup;
+type NavItemOrGroup = NavItem | NavGroup
 
 const navStructure: NavItemOrGroup[] = [
   { name: 'Consignments', path: '/consignments', icon: <DashboardIcon className="w-5 h-5" /> },
@@ -25,27 +25,26 @@ const navStructure: NavItemOrGroup[] = [
 ]
 
 function isNavGroup(item: NavItemOrGroup): item is NavGroup {
-  return 'items' in item;
+  return 'items' in item
 }
 
 interface SidebarProps {
-  isExpanded: boolean;
-  onToggle: () => void;
+  isExpanded: boolean
+  onToggle: () => void
 }
 
 export function Sidebar({ isExpanded, onToggle }: SidebarProps) {
-  const location = useLocation();
-  const { role } = useRole();
-  const [isHovered, setIsHovered] = useState(false);
-  const [expandedGroups, setExpandedGroups] = useState<Set<string>>(new Set());
-  const previousPathRef = useRef<string>(location.pathname);
+  const location = useLocation()
+  const { role } = useRole()
+  const [isHovered, setIsHovered] = useState(false)
+  const [expandedGroups, setExpandedGroups] = useState<Set<string>>(new Set())
+  const previousPathRef = useRef<string>(location.pathname)
 
   const filteredNavStructure = useMemo(() => {
     return navStructure
       .filter((item) => {
         // If item has roles defined, check if current role is included
-        return !(item.roles && !item.roles.includes(role));
-
+        return !(item.roles && !item.roles.includes(role))
       })
       .map((item) => {
         // If it's a group, also filter its nested items
@@ -53,52 +52,51 @@ export function Sidebar({ isExpanded, onToggle }: SidebarProps) {
           return {
             ...item,
             items: item.items.filter((child) => !child.roles || child.roles.includes(role)),
-          };
+          }
         }
-        return item;
+        return item
       })
       .filter((item) => {
         // If a group has no items left after filtering, hide the group itself
-        return !(isNavGroup(item) && item.items.length === 0);
-
-      });
-  }, [role]);
+        return !(isNavGroup(item) && item.items.length === 0)
+      })
+  }, [role])
 
   // Determine if sidebar should show expanded content
-  const showExpanded = isExpanded || (!isExpanded && isHovered);
+  const showExpanded = isExpanded || (!isExpanded && isHovered)
 
   // Auto-expand groups that contain the active page
   useEffect(() => {
     if (previousPathRef.current !== location.pathname) {
-      previousPathRef.current = location.pathname;
-      const groupsToExpand = new Set<string>();
+      previousPathRef.current = location.pathname
+      const groupsToExpand = new Set<string>()
       filteredNavStructure.forEach((item) => {
         if (isNavGroup(item)) {
-          const hasActivePath = item.items.some((child) => child.path === location.pathname);
+          const hasActivePath = item.items.some((child) => child.path === location.pathname)
           if (hasActivePath) {
-            groupsToExpand.add(item.name);
+            groupsToExpand.add(item.name)
           }
         }
-      });
+      })
       // eslint-disable-next-line react-hooks/set-state-in-effect
-      setExpandedGroups(groupsToExpand);
+      setExpandedGroups(groupsToExpand)
     }
-  }, [location.pathname, filteredNavStructure]);
+  }, [location.pathname, filteredNavStructure])
 
   const toggleGroup = (groupName: string) => {
     setExpandedGroups((prev) => {
-      const next = new Set(prev);
+      const next = new Set(prev)
       if (next.has(groupName)) {
-        next.delete(groupName);
+        next.delete(groupName)
       } else {
-        next.add(groupName);
+        next.add(groupName)
       }
-      return next;
-    });
-  };
+      return next
+    })
+  }
 
   const renderNavItem = (item: NavItem, isInGroup = false) => {
-    const isActive = location.pathname === item.path;
+    const isActive = location.pathname === item.path
     return (
       <Link
         key={item.path}
@@ -109,29 +107,31 @@ export function Sidebar({ isExpanded, onToggle }: SidebarProps) {
             ? 'bg-primary-500 text-white shadow-md'
             : 'text-primary-100 hover:bg-primary-800/50 hover:text-white',
           !showExpanded && 'justify-center',
-          isInGroup && showExpanded && 'ml-4 text-sm'
+          isInGroup && showExpanded && 'ml-4 text-sm',
         )}
         title={!showExpanded ? item.name : undefined}
       >
         <span className="flex items-center text-xl shrink-0">{item.icon}</span>
         {showExpanded && <span className="text-[15px] whitespace-nowrap">{item.name}</span>}
       </Link>
-    );
-  };
+    )
+  }
 
   const renderNavGroup = (group: NavGroup) => {
-    const isGroupExpanded = expandedGroups.has(group.name);
-    const hasActivePath = group.items.some((item) => item.path === location.pathname);
+    const isGroupExpanded = expandedGroups.has(group.name)
+    const hasActivePath = group.items.some((item) => item.path === location.pathname)
 
     if (!showExpanded) {
       // Collapsed sidebar: show group header and expanded sub-items with shared background
       return (
         <div key={group.name} className="flex flex-col gap-1">
           {/* Wrapper with shaded background if expanded */}
-          <div className={clsx(
-            'flex flex-col gap-1 rounded-md transition-all',
-            isGroupExpanded && 'bg-primary-500/20 p-1'
-          )}>
+          <div
+            className={clsx(
+              'flex flex-col gap-1 rounded-md transition-all',
+              isGroupExpanded && 'bg-primary-500/20 p-1',
+            )}
+          >
             {/* Group header */}
             <button
               onClick={() => toggleGroup(group.name)}
@@ -141,7 +141,7 @@ export function Sidebar({ isExpanded, onToggle }: SidebarProps) {
                   ? 'text-white hover:bg-primary-500/40 border-primary-400/30'
                   : hasActivePath
                     ? 'bg-primary-500/30 text-white border-primary-400/20'
-                    : 'text-primary-100 hover:bg-primary-800/50 hover:text-white border-transparent hover:border-primary-600/20'
+                    : 'text-primary-100 hover:bg-primary-800/50 hover:text-white border-transparent hover:border-primary-600/20',
               )}
               title={group.name}
             >
@@ -150,33 +150,34 @@ export function Sidebar({ isExpanded, onToggle }: SidebarProps) {
               <ChevronDownIcon
                 className={clsx(
                   'w-3 h-3 absolute bottom-1 right-1 transition-transform',
-                  isGroupExpanded ? 'rotate-0' : '-rotate-90'
+                  isGroupExpanded ? 'rotate-0' : '-rotate-90',
                 )}
               />
             </button>
 
             {/* Expanded sub-items */}
-            {isGroupExpanded && group.items.map((item) => {
-              const isActive = location.pathname === item.path;
-              return (
-                <Link
-                  key={item.path}
-                  to={item.path}
-                  className={clsx(
-                    'flex items-center justify-center px-3 h-12 min-h-12 shrink-0 rounded-md transition-all',
-                    isActive
-                      ? 'bg-primary-500 text-white shadow-md'
-                      : 'text-primary-100 hover:bg-primary-500/40 hover:text-white'
-                  )}
-                  title={item.name}
-                >
-                  <span className="flex items-center text-xl shrink-0">{item.icon}</span>
-                </Link>
-              );
-            })}
+            {isGroupExpanded &&
+              group.items.map((item) => {
+                const isActive = location.pathname === item.path
+                return (
+                  <Link
+                    key={item.path}
+                    to={item.path}
+                    className={clsx(
+                      'flex items-center justify-center px-3 h-12 min-h-12 shrink-0 rounded-md transition-all',
+                      isActive
+                        ? 'bg-primary-500 text-white shadow-md'
+                        : 'text-primary-100 hover:bg-primary-500/40 hover:text-white',
+                    )}
+                    title={item.name}
+                  >
+                    <span className="flex items-center text-xl shrink-0">{item.icon}</span>
+                  </Link>
+                )
+              })}
           </div>
         </div>
-      );
+      )
     }
 
     // Expanded sidebar: show group header with expand/collapse
@@ -188,29 +189,27 @@ export function Sidebar({ isExpanded, onToggle }: SidebarProps) {
             'flex items-center gap-4 px-3 h-12 min-h-12 shrink-0 rounded-md font-medium transition-all w-full',
             hasActivePath && isGroupExpanded
               ? 'bg-primary-500/20 text-white'
-              : 'text-primary-100 hover:bg-primary-800/50 hover:text-white'
+              : 'text-primary-100 hover:bg-primary-800/50 hover:text-white',
           )}
         >
           <span className="flex items-center text-xl shrink-0">{group.icon}</span>
           <span className="text-[15px] whitespace-nowrap flex-1 text-left">{group.name}</span>
           <ChevronDownIcon
-            className={clsx(
-              'w-4 h-4 transition-transform',
-              isGroupExpanded ? 'rotate-0' : '-rotate-90'
-            )}
+            className={clsx('w-4 h-4 transition-transform', isGroupExpanded ? 'rotate-0' : '-rotate-90')}
           />
         </button>
         {isGroupExpanded && (
           <div className="flex flex-col gap-1">{group.items.map((item) => renderNavItem(item, true))}</div>
         )}
       </div>
-    );
-  };
+    )
+  }
 
   return (
     <aside
-      className={`${showExpanded ? 'w-64' : 'w-20'
-        } h-[calc(100vh-64px)] bg-linear-to-b from-primary-900 to-primary-950 text-white flex flex-col fixed left-0 top-16 border-r border-primary-800/30 shadow-xl transition-all duration-300 z-20`}
+      className={`${
+        showExpanded ? 'w-64' : 'w-20'
+      } h-[calc(100vh-64px)] bg-linear-to-b from-primary-900 to-primary-950 text-white flex flex-col fixed left-0 top-16 border-r border-primary-800/30 shadow-xl transition-all duration-300 z-20`}
       onMouseEnter={() => !isExpanded && setIsHovered(true)}
       onMouseLeave={() => !isExpanded && setIsHovered(false)}
     >
@@ -218,9 +217,9 @@ export function Sidebar({ isExpanded, onToggle }: SidebarProps) {
       <nav className="flex-1 p-3 flex flex-col gap-1 overflow-y-auto">
         {filteredNavStructure.map((item) => {
           if (isNavGroup(item)) {
-            return renderNavGroup(item);
+            return renderNavGroup(item)
           }
-          return renderNavItem(item);
+          return renderNavItem(item)
         })}
       </nav>
 
@@ -248,28 +247,22 @@ export function Sidebar({ isExpanded, onToggle }: SidebarProps) {
         <div className="px-4 pb-4">
           <button
             onClick={onToggle}
-            className={`${showExpanded ? 'w-full' : 'w-10'
-              } h-10 rounded-full bg-primary-500 hover:bg-primary-600 flex items-center ${showExpanded ? 'justify-between px-4' : 'justify-center'
-              } text-white transition-all shadow-lg`}
-            title={isExpanded ? "Collapse sidebar" : "Expand sidebar"}
+            className={`${
+              showExpanded ? 'w-full' : 'w-10'
+            } h-10 rounded-full bg-primary-500 hover:bg-primary-600 flex items-center ${
+              showExpanded ? 'justify-between px-4' : 'justify-center'
+            } text-white transition-all shadow-lg`}
+            title={isExpanded ? 'Collapse sidebar' : 'Expand sidebar'}
           >
-            {showExpanded && (
-              <span className="text-sm font-medium">
-                {isExpanded ? 'Collapse' : 'Expand'}
-              </span>
-            )}
-            {isExpanded ? (
-              <ChevronLeftIcon className="w-5 h-5" />
-            ) : (
-              <ChevronRightIcon className="w-5 h-5" />
-            )}
+            {showExpanded && <span className="text-sm font-medium">{isExpanded ? 'Collapse' : 'Expand'}</span>}
+            {isExpanded ? <ChevronLeftIcon className="w-5 h-5" /> : <ChevronRightIcon className="w-5 h-5" />}
           </button>
         </div>
       </div>
     </aside>
-  );
+  )
 }
 
 function clsx(...classes: (string | boolean | undefined)[]) {
-  return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(' ')
 }